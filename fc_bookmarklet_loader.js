01baa15f5e39695a755724be8ebb8a65350919f0
Blake Price <bprice@justin.tv>
2019-01-30 13:32:41 -0800

    Init

---
+++ fc_bookmarklet_loader.js
@@ -0,0 +1,5 @@
+javascript:(
+    function () {
+        Game.LoadMod('https://raw.githubusercontent.com/nknowlton/FrozenCookies/master/frozen_cookies.js');
+    }()
+);
---
+++ fc_userscript_loader.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name           Frozen Cookies
+// @version        github-latest
+// @description    Userscript to load Frozen Cookies written by Icehawk78, forked by Lordshinjo
+// @author         Lordshinjo
+// @homepage       https://github.com/Lordshinjo/FrozenCookies
+// @include        http://orteil.dashnet.org/cookieclicker/
+// @updateURL      https://rawgit.com/Lordshinjo/FrozenCookies/master/fc_userscript_loader.js
+// @downloadURL    https://rawgit.com/Lordshinjo/FrozenCookies/master/fc_userscript_loader.js
+// @run-at         document-start
+// ==/UserScript==
+
+// Dev:       https://github.com/Lordshinjo/FrozenCookies/development/
+// Master:    https://github.com/Lordshinjo/FrozenCookies/master/
+// Github.io: http://lordshinjo.github.io/FrozenCookies/
+
+function LoadFrozenCookies() {
+    Game.LoadMod('https://raw.githubusercontent.com/nknowlton/FrozenCookies/master/frozen_cookies.js');
+}
+
+window.addEventListener("load", LoadFrozenCookies, false);
